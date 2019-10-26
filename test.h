60df72e95cd4404ff24901b295f56137f9d65c45
Elderly-AI <56202288+Elderly-AI@users.noreply.github.com>
2019-10-26 10:12:10 +0300

    Update test.h

--- test.h
+++ test.h
@@ -2,9 +2,8 @@
 
 size_t read_array(char ***array, const char *file_name);
 
-bool emotion_test(const char** array, const size_t array_size,
-const char *good_pattern, const char *bad_pattern);
+bool emotion_test(const char **array, const size_t array_size,
+                  const char *good_pattern, const char *bad_pattern);
 
-bool fork_emotion_test(const char** array, const size_t array_size,
-const char *good_pattern, const char *bad_pattern);
-
+bool fork_emotion_test(const char **array, const size_t array_size,
+                       const char *good_pattern, const char *bad_pattern);
