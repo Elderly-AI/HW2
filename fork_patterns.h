c643f0889bf2fc6149abc7f8edefd9ee3549ecc2
Elderly-AI <56202288+Elderly-AI@users.noreply.github.com>
2019-10-26 10:05:02 +0300

    Update fork_patterns.h

--- fork_patterns.h
+++ fork_patterns.h
@@ -1,4 +1,4 @@
 //#include "fork_patterns.h"
 
-unsigned long long int fork_pattern_entity_in_array(
+int64_t fork_pattern_entity_in_array(
 const size_t core_count, const char **array, const size_t array_size, const char *pattern);
