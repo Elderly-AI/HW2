34e5987beaf22a3e8304b68e760305c732c19804
Elderly-AI <56202288+Elderly-AI@users.noreply.github.com>
2019-10-26 10:07:56 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,10 +1,10 @@
 /*Условие ИЗ№2:
 Сравните и выведите в консоль время работы последовательного и параллельного
-с использованием нескольких процессов алгоритмов, каждый из которых выделяет 
-в динамической памяти символьный массив размером 100 Мб и, рассматривая его 
-содержимое как абстрактную переписку, определяет эмоциональную окраску последней. 
-Переписка считается оптимистичной, если диграфов :) в ней больше, чем диграфов :(; 
-в противном случае переписка признается пессимистичной.
+с использованием нескольких процессов алгоритмов, каждый из которых выделяет
+в динамической памяти символьный массив размером 100 Мб и, рассматривая его
+содержимое как абстрактную переписку, определяет эмоциональную окраску
+последней. Переписка считается оптимистичной, если диграфов :) в ней больше, чем
+диграфов :(; в противном случае переписка признается пессимистичной.
 */
 
 #define SIZE_T_LINE_LENGTH 128
@@ -13,116 +13,112 @@
 
 #include <assert.h>
 #include <errno.h>
-#include <stdbool.h> 
+#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <string.h>
 
 #include "patterns.h"
 #include "test.h"
 
+size_t read_array(char ***array, const char *file_name) {
+  if (file_name == NULL) {
+    exit(EINVAL);
+  }
 
+  FILE *fp;
+  size_t array_size = 1;
+  size_t line_size = SIZE_T_LINE_LENGTH;
 
-size_t read_array(char ***array, const char *file_name){
-	
-	if(file_name == NULL){
-		exit(EINVAL);
-	}
+  *array = (char **)malloc(array_size * sizeof(char *));
 
-	FILE *fp;
-	size_t array_size = 1;
-	size_t line_size = SIZE_T_LINE_LENGTH;
+  if (*array == NULL) {
+    exit(ENOMEM);
+  }
 
-	*array = (char**) malloc (array_size * sizeof(char*));
+  for (size_t c = 0; c < array_size; ++c) {
+    (*array)[c] = (char *)malloc(line_size * sizeof(char));
 
-	if(*array == NULL){
-		exit(ENOMEM);
-	}
+    if ((*array)[c] == NULL) {
+      for (size_t index = 0; index < c; ++index) {
+        free(array[index]);
+      }
+      free(*array);
 
-	for(size_t c = 0; c < array_size; ++c){
-		(*array)[c] = (char*) malloc (line_size * sizeof(char));
+      exit(ENOMEM);
+    }
+  }
 
-		if((*array)[c] == NULL){
-			for(size_t index = 0; index < c; ++index){
-				free(array[index]);
-			}
-			free(*array);
+  if ((fp = fopen(file_name, "r")) == NULL) {
+    for (size_t c = 0; c < array_size; ++c) {
+      free((*array)[c]);
+    }
+    free(*array);
 
-			exit(ENOMEM);
-		}	
-	}
-	
-	if ((fp = fopen(file_name, "r")) == NULL){
-		for(size_t c = 0; c < array_size; ++c){
-			free((*array)[c]);
-		}
-		free(*array);
+    exit(EAGAIN);
+  }
 
-	 	exit(EAGAIN);
-	}
+  for (size_t c = 0;
+       fscanf(fp, "%" CHAR_LINE_LENGTH "s", (*array)[c]) != EOF;) {
+    c++;
 
-	for(size_t c = 0; fscanf(fp, "%"CHAR_LINE_LENGTH"s", (*array)[c]) != EOF;){
-		c++;
+    if (c == array_size) {
+      char **buff = (char **)realloc(*array, array_size * 2 * sizeof(char *));
+      if (buff == NULL) {
+        for (size_t index = 0; index < array_size; ++index) {
+          free((*array)[index]);
+        }
+        free(*array);
 
-		if(c == array_size){
-			char **buff = (char **) realloc (*array, array_size * 2 * sizeof(char*));
-			if(buff == NULL){
-				for(size_t index = 0; index < array_size; ++index){
-					free((*array)[index]);
-				}
-				free(*array);
+        exit(ENOMEM);
+      }
 
-				exit(ENOMEM);
-			}
+      *array = buff;
 
-			*array = buff;
+      for (size_t g = c; g < array_size * 2; ++g) {
+        (*array)[g] = (char *)malloc(line_size * sizeof(char));
 
-			for(size_t g = c; g < array_size * 2; ++g){
-				(*array)[g] = (char*) malloc (line_size * sizeof(char));
-				
-				if((*array)[g] == NULL){
-					for(size_t index = 0; index < g; ++index){
-						free((*array)[index]);
-					}
-					free(*array);
+        if ((*array)[g] == NULL) {
+          for (size_t index = 0; index < g; ++index) {
+            free((*array)[index]);
+          }
+          free(*array);
 
-					exit(ENOMEM);
-				}	
-			}
+          exit(ENOMEM);
+        }
+      }
 
-			array_size *= 2;
-		}
-	}
-	
-	return(array_size);
+      array_size *= 2;
+    }
+  }
+
+  return (array_size);
 }
 
-bool emotion_test(const char** array, const size_t array_size,
-const char *good_pattern, const char *bad_pattern){
-
-	return((long long int)(pattern_entity_in_array(array, array_size, good_pattern) -
-		    (long long int)(pattern_entity_in_array(array, array_size, bad_pattern))) > 0);
+bool emotion_test(const char **array, const size_t array_size,
+                  const char *good_pattern, const char *bad_pattern) {
+  return ((int64_t)(pattern_entity_in_array(array, array_size, good_pattern) -
+                    (int64_t)(pattern_entity_in_array(array, array_size,
+                                                      bad_pattern))) > 0);
 }
 
-int main(){
+int main() {
+  char **array;
+  size_t array_size = 0;
 
-	char **array;
-	size_t array_size = 0;
+  array_size = read_array(&array, "test1.txt");
+  assert(emotion_test((const char **)array, array_size, ":)", ":("));
+  printf("Test 1 passed SUCCESS\n");
+  for (size_t c = 0; c < array_size; ++c) {
+    free(array[c]);
+  }
+  free(array);
 
-	array_size = read_array(&array, "test1.txt");
-	assert(emotion_test((const char**)array, array_size, ":)", ":("));
-	printf("Test 1 passed SUCCESS\n");
-	for(size_t c = 0; c < array_size; ++c){
-		free(array[c]);
-	}
-	free(array);
-
-	array_size = read_array(&array, "test2.txt");
-	assert(!emotion_test((const char**)array, array_size, ":)", ":("));
-	printf("Test 2 passed SUCCESS\n");
-	for(size_t c = 0; c < array_size; ++c){
-		free(array[c]);
-	}
-	free(array);
-
+  array_size = read_array(&array, "test2.txt");
+  assert(!emotion_test((const char **)array, array_size, ":)", ":("));
+  printf("Test 2 passed SUCCESS\n");
+  for (size_t c = 0; c < array_size; ++c) {
+    free(array[c]);
+  }
+  free(array);
 }
