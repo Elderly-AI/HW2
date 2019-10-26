cccff7ecff7467782b9b15829c2df54ae2048b2a
Elderly-AI <56202288+Elderly-AI@users.noreply.github.com>
2019-10-26 10:07:05 +0300

    Update patterns.h

--- patterns.h
+++ patterns.h
@@ -6,5 +6,5 @@ const char *pattern, const size_t pattern_len);
 unsigned int pattern_entity_in_slice(const char *f_array, 
 const char *s_array, const char *pattern, const size_t pattern_len);
 
-unsigned long long int pattern_entity_in_array(const char **array, 
+int64_t pattern_entity_in_array(const char **array, 
 const size_t array_size, const char *pattern);
